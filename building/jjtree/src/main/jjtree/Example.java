8c30c61965f0fde2c5a97a6e18cb8fed622e7f09
phoet <phoetmail@googlemail.com>
2009-11-19 20:56:42 +0100

    move to package

--- building/jjtree/src/main/jjtree/Example.java
+++
@@ -1,9 +0,0 @@
-import java.lang.annotation.*;
- 
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.PACKAGE, ElementType.FIELD})
-public @interface Example {
-    String value();
-    Priority priority() default Priority.MEDIUM;
-}
