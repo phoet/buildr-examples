46c0b1958f4d05800035ea030d84eb6c9eff3b75
phoet <phoetmail@googlemail.com>
2009-11-30 20:58:43 +0100

    some more install and upload

--- artifacts/install_upload/run.sh
+++ artifacts/install_upload/run.sh
@@ -1,2 +1,8 @@
-echo "build using local repo-task"
-buildr
+echo "just build"
+buildr clean build
+echo "install an artifact"
+buildr clean install
+echo "upload an artifact"
+buildr upload
+echo "install directly using task"
+buildr clean install_upload:install_directly
