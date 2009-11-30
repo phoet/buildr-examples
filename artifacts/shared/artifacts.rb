5c262533f6f5fd1a68cbba2f3fcbdb0bdee8c450
phoet <phoetmail@googlemail.com>
2009-11-30 20:11:18 +0100

    add

---
+++ artifacts/shared/artifacts.rb
@@ -0,0 +1,5 @@
+SHARED_LIB = struct(
+  :activation  =>'javax.activation:activation:jar:1.1',
+  :persistence =>'javax.persistence:persistence-api:jar:1.0',
+  :stream      =>'stax:stax-api:jar:1.0.1'
+)
