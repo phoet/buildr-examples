2e4a31ed8270cf7c7091ee0720584285a55d6965
phoet <phoetmail@googlemail.com>
2009-12-01 21:12:27 +0100

    rename

--- packaging/multiple/run.sh
+++ packaging/multiple/run.sh
@@ -1,6 +1,6 @@
 echo "package to create project artifacts"
 buildr clean package
 echo "show some artifacts"
-unzip -l target/simple-1.0.0.jar
-unzip -l target/simple-1.0.0.war
-unzip -l target/simple.zip
+unzip -l target/multiple-1.0.0.jar
+unzip -l target/multiple-1.0.0.war
+unzip -l target/multiple.zip
