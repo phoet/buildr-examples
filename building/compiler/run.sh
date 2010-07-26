68e3243114fd5dc8d100163c21f8e1a54cc90551
phoet <phoetmail@googlemail.com>
2010-07-26 17:56:10 +0200

    dunno if these build-options do something...

--- building/compiler/run.sh
+++ building/compiler/run.sh
@@ -1,6 +1,9 @@
+echo "compile and enable debug ..."
+buildr clean compile debug=on
+
 echo "compile and disable debug ..."
 echo "... when calling buildr with debug=off"
-buildr compile debug=off
+buildr clean compile debug=off
 
 echo "... once until we change the variable with sysproperty"
 export DEBUG=off
