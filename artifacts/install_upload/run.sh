33841dbe33ce47f17ba73a32f23badf376b6e39d
phoet <phoetmail@googlemail.com>
2010-07-26 17:44:46 +0200

    add chmod and use local path to home

--- artifacts/install_upload/run.sh
+++ artifacts/install_upload/run.sh
@@ -3,6 +3,7 @@ buildr clean build
 echo "install an artifact"
 buildr clean install
 echo "upload an artifact"
+chmod -R +w ~/.m2/repository/
 buildr upload
 echo "install directly using task"
 buildr clean install_upload:install_directly
