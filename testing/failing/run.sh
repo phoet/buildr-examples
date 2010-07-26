dfce5834cdf921eb017d173896c35375f0e52495
phoet <phoetmail@googlemail.com>
2010-07-26 18:13:05 +0200

    notice to failing test and annotate test extending

--- testing/failing/run.sh
+++ testing/failing/run.sh
@@ -1,5 +1,7 @@
-echo "package and execute tests"
+echo "package and execute tests that should fail..."
+echo "...failing tests EXPECTED:"
 buildr clean package
+echo "...end of failing tests"
 
 echo "package and don't execute tests"
 buildr clean package test=no
